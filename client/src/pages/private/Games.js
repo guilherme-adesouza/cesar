a9dd3cf3eee34af296d1bea772a6edcf161bc3cf
guilherme-adesouza <guilherme.souza2@universo.univates.br>
2019-06-08 16:52:55 -0300

    closes #3 CRUD Jogos

--- client/src/pages/private/Games.js
+++ client/src/pages/private/Games.js
@@ -3,12 +3,58 @@ import './Games.css';
 import React, {Component} from 'react';
 
 import Table from '../../components/Table';
+import Button from '../../components/Button';
 import GameService from '../../service/GameService';
+
+import {csYup} from '../../components/form/csYup';
+import { Formik, Form } from 'formik';
+import Field from '../../components/form/Field';
+import CSButton from '../../components/form/CSButton';
+
+const GameSchema = csYup(yup => {
+  return yup.object().shape({
+    name: yup.string().required().default(''),
+    name_visualization: yup.string().default('')
+  })
+});
+
+class GameForm extends Component {
+
+  saveGames = async (values, actions) => {
+    try {
+      await new GameService().create(values);
+      this.props.onSubmit();
+    } catch(e) {
+      console.error('error trying to create plataform...', e);
+    }
+  }
+
+  initGame = GameSchema.default();
+
+  render(){
+    return (
+      <Formik
+        validationSchema={GameSchema}
+        initialValues={this.initGame}
+        onSubmit={this.saveGames}>
+        <Form>
+          <div className="Form">
+            <Field title="Nome" type="text" name="name" required={true}/>
+            <Field title="Nome de visualização" type="text" name="name_visualization"/>
+            <CSButton type="submit" className="Dark">Salvar</CSButton>
+          </div>
+        </Form>
+      </Formik>
+    )
+  }
+}
 
 class GamesPage extends Component {
 
   state = {
     loading: true,
+    onForm: false,
+    clearForm: false,
     games: [],
   }
 
@@ -17,7 +63,7 @@ class GamesPage extends Component {
   }
 
   getGamesList = async () => {
-    this.setState({loading: true});
+    this.setState({onForm: false, loading: true});
     try {
       const games = await new GameService().getAll();
       this.setState({games});
@@ -28,17 +74,28 @@ class GamesPage extends Component {
     }
   }
 
+  newGame = (event) => {
+    event.preventDefault();
+    this.setState({onForm: !this.state.onForm, clearForm: this.state.onForm});
+  }
 
   render(){
-    const {loading, games} = this.state;
+    const {loading, onForm, clearForm, games} = this.state;
 
     if(loading) return null;
     return (
       <div className="Plataform">
         <div className="ContentTitle">
           <h2 className="Title">Games</h2>
+          <Button className="ThemeBackground" onClick={this.newGame}>{this.state.onForm ? "Voltar" : "Novo"}</Button>
         </div>
-        <Table data={games} object="jogo"/>
+        {onForm ?
+          <GameForm
+            clearForm={clearForm}
+            onSubmit={this.getGamesList}
+          />
+        : <Table data={games} object="jogo"/>
+        }
       </div>
     )
   }
